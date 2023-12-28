8a7a4813d27e5548edf27cd81fea153f0f94903b
sarAmgad <sarahelmy56@gmail.com>
2023-12-28 20:47:55 +0200

    no js

--- src/routers/userRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var userController = require("../controllers/userController");
-var router = express_1.default.Router();
-router.get('/', userController.getAllUsers);
-router.get('/userId/:userId', userController.getUserById);
-router.get('/userName/:userName', userController.getUserByUserName);
-exports.default = router;
